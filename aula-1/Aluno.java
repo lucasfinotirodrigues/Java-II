71babf15e7242fb167532c0bcb3cc13357554f9e
Lucas Finoti Rodrigues <lucasfinotirodrigues@hotmail.com>
2023-08-15 18:57:42 +0000

    eat: file of first contact added

---
+++ aula-1/Aluno.java
@@ -0,0 +1,22 @@
+public class Aluno {
+    private String nome;
+    private int idade;
+
+    //Geters and Seters of Nome
+    public void setNome(String nome){
+        this.nome = nome
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+    
+    //Geters and Seters of Idade
+    public int void setIdade(int idade){
+        this.idade = idade;
+    }
+
+    public int getIdade(){
+        return this.idade;
+    }
+}
